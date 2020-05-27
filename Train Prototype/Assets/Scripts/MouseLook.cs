3115b5c6249228a3ddb9622402b7c5d50ff76c40
Jackson <jlm0135@auburn.edu>
2020-05-27 15:48:20 -0500

    I put a player in the game
    The player is just a cylinder, but he's a good boy (for now). His name is Dylan.
    
    The player is quite big compared to the train, but that can be fixed a number of ways (bigger train, smaller player, etc.). Also because programming is funny, if you jump as the player, you also stop the train hahahahahahahahahahahahahahahaha. Easy fix, don't care to do it now though.
    
    I made both our old train camera and our new player object into prefabs that can be put into the scene and enabled/disabled however one pleases. Since both items are technically a camera, only one should be enabled at a time.
    
    For the player, if you start the game, you will notice that the cursor disappears, meaning you will not be allowed to use the editor while the game is playing... is what I WOULD SAY if I was a stupid dumb idiot brain. Luckily, I added some code that allows the player to left click to get their mouse cursor back, if this is done, the user also loses control of their player object so no funky business happens while using the editor. You can also left click on the game window to regain control of the player. Very cool very epic definitely not thinking about how the train itself is still broken no siree

---
+++ Train Prototype/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public Transform playerBody;
+
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // At the start of the game, our cursor is not shown and we have
+        // control of our player. Change this to CursorLockMode.None
+        // to give us control of the editor at the start of the game.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Left Click to unlock or lock the mouse cursor
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+
+        // If our cursor is locked, we have control of the camera
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+
+    }
+}
---
+++ Train Prototype/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
+
+    public float speed = 12f;
+    public float gravity = -19.62f;
+    public float groundDistance = 0.4f;
+    public float jumpHeight = 3f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if we're near the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // If we are on the ground, reset our velocity to a small number
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jumping code. Funnily enough, because of the way this game is coded,
+        // if we choose to jump, this also stops the train. Hehehehehehe
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
