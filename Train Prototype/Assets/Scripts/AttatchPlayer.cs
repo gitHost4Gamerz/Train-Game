a3bd962034a95a49509f9b5c2674c73eb52f43be
Jackson <jlm0135@auburn.edu>
2020-05-30 21:54:27 -0500

    New Player That Can Ride The Train
    I kinda sorta yoinked some funky movement code online to test with a rigid body to see if that allows us to ride the train and it DOES. So now the next step is to iron out some of the kinks and then write our own movement code for our player very cool very epic style

---
+++ Train Prototype/Assets/Scripts/AttatchPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class AttatchPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            other.transform.parent = null;
+        }
+    }
+}
