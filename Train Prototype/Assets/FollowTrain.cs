f47ac4da3bac24af25c3cacafa563c58d03e1154
Charles Painter <cpainter307eso@aol.com>
2020-06-10 12:54:00 -0500

    New Demo Area and Models
    Created a large and in-charge new demo area for continued work which includes slopes, track swapping, and multi-directional paths. 
    
    Also added in some new models as prefabs that we can populate that map with, like some buildings, hills, and trees. I've made a little town, started work on a forest, and added a big hill in.

--- Train Prototype/Assets/FollowTrain.cs
+++ Train Prototype/Assets/FollowTrain.cs
@@ -6,9 +6,11 @@ public class FollowTrain : MonoBehaviour
     public Transform train;
     public Vector3 offset;
 
+
     // Update is called once per frame
     void Update()
     {
         transform.position = train.position + offset;
+        transform.LookAt(train);
     }
 }
